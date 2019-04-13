d1d04feff4f95777c4075605b7e6bea02e6bd4d8
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-04-13 22:05:25 +0900

    config simple collectionview

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -4,4 +4,16 @@ class ViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Cannot deqeue person cell")
+        }
+
+        return cell
+    }
 }
