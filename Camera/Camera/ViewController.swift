c9c042fad599c11899bc8e3c651fe8a8f78b3910
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-04-26 14:47:38 +0900

    add title to view

--- Camera/Camera/ViewController.swift
+++ Camera/Camera/ViewController.swift
@@ -3,5 +3,12 @@ import UIKit
 class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupView()
+    }
+
+    func setupView() {
+        title = "Camera App"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
