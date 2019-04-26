59a3ab5130dac7458fbee8a5d99ce46b6f29cebc
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-04-26 14:45:22 +0900

    use viewcontroller as base vc

--- Camera/Camera/ViewController.swift
+++ Camera/Camera/ViewController.swift
@@ -1,20 +1,7 @@
-//
-//  ViewController.swift
-//  Camera
-//
-//  Created by Anton Van Eechaute on 4/26/19.
-//  Copyright © 2019 Anton Van Eechaute. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
