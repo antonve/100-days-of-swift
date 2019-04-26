4093ee39d0b4cdae77e6c39c59ae751b8ab80be6
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-04-26 14:42:37 +0900

    add initial project for camera challenge

---
+++ Camera/Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by Anton Van Eechaute on 4/26/19.
+//  Copyright © 2019 Anton Van Eechaute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
