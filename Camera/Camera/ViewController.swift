4acf4f57e60a78d6c4994024a52176f0708ba949
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-04-26 14:54:45 +0900

    configure tableviewcontroller

--- Camera/Camera/ViewController.swift
+++ Camera/Camera/ViewController.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var pictures: [Picture] = [Picture]()
+    let cellIdentifier = "Cell"
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -10,5 +13,18 @@ class ViewController: UITableViewController {
     func setupView() {
         title = "Camera App"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) else { fatalError() }
+        cell.textLabel?.text = pictures[indexPath.row].caption
+
+        return cell
     }
 }
