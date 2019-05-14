6367ba2d4fe88f77159bf148403368bc6cfcef69
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-05-14 12:28:07 +0900

    update layout

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -1,7 +1,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+
+    @IBAction func save(_ sender: Any) {
+    }
+
+    @IBAction func intensityChanged(_ sender: Any) {
     }
 }
